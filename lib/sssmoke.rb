6c0b64c212b5151ac2f526581f92cababe5f85a9
Paolo "Nusco" Perrotta <paolo.nusco.perrotta@gmail.com>
2012-12-15 21:40:54 +0100

    deal with 404s

--- lib/sssmoke.rb
+++ lib/sssmoke.rb
@@ -1,1 +1,35 @@
 require "sssmoke/version"
+
+module Sssmoke
+  FOUR_O_FOUR = [404, "404 - Page not found"]
+  require 'sinatra/base'
+
+  class Base < Sinatra::Base
+    require 'erb'
+    require 'tilt'
+    
+    def show(template)
+      template = "#{template}.erb" unless template.end_with? '.erb'
+      File.exist?(template) ? Tilt::ERBTemplate.new(template).render : FOUR_O_FOUR
+    end
+  end
+  
+  def self.app
+    filename = ARGV.shift || "."
+
+    if File.directory? filename
+      Class.new(Sssmoke::Base) do
+        set :views, filename
+        get('/') { show 'index' }
+        get('/:template') {|template| show template }
+      end
+    else
+      Class.new(Sssmoke::Base) do
+        get('/') { show filename }
+        error(404) { FOUR_O_FOUR }
+      end
+    end
+  end
+  
+  app.run!
+end
